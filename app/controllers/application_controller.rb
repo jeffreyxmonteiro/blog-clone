1cc68051921a6cf3910fd0d8f1c1b0ef9781965e
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-06-11 18:41:40 -0400

    Added first name and last name to user model, updated sign up form, included parameters in application controller, created about contact pages and included links to both in nav.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+
+  protected
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname])
+  end
 end
