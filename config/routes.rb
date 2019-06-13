1270afc65705a369c915e78ffb206db6c4cd863e
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-06-12 22:36:37 -0400

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+
+  unauthenticated :user do
+    root "pages#home"
+  end
+
+
   get 'contact', to: 'pages#contact', as: :contact
   get 'about', to: 'pages#about', as: :about
+
+
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
