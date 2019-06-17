e71428e464f4a23ca923d3d81b0c04f7ab7c6abf
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-06-16 20:39:15 -0400

    Included pg search scope for Snipps model.

--- app/models/snipp.rb
+++ app/models/snipp.rb
@@ -1,4 +1,6 @@
 class Snipp < ApplicationRecord
+  include PgSearch
+  pg_search_scope :search_snipps, against: [ :quotation, :quoter ]
   belongs_to :user
   validates_presence_of :medium, :quotation, :takeaway, :quoter, :source
 end
