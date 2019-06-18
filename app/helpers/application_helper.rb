cabbbb0de621abd5c9c504d62b90e5edb1cfea2f
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-06-18 15:26:55 -0400

    Created initial helper method for medium selection.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def medium_helper medium
+    if medium == "Podcast"
+    elsif medium == "Tweet"
+    elsif medium == "Book"
+    elsif medium == "Video"
+    elsif medium == "Online Article"
+    elsif medium == "Newsletter"
+    elsif medium == "Other"
+    end
+  end
 end
+
+
+# ["Podcast", "Tweet", "Book", "Video", "Online Article", "Newsletter", "Other"]
