87930d28ba8418631d7fcf5b57c50334012d3b83
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-06-18 15:48:28 -0400

    Completed initial version of medium helper method and implemented to user show page. Minor design updates to card e.g. box shadow, spacing.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,19 @@ module ApplicationHelper
 
   def medium_helper medium
     if medium == "Podcast"
+      image_tag("podcast-float.png", class: "medium-icon")
     elsif medium == "Tweet"
+      image_tag("twitter-float.png", class: "medium-icon")
     elsif medium == "Book"
+      image_tag("book-float.png", class: "medium-icon")
     elsif medium == "Video"
+      image_tag("youtube-float.png", class: "medium-icon")
     elsif medium == "Online Article"
+      image_tag("article-float.png", class: "medium-icon")
     elsif medium == "Newsletter"
+      image_tag("mail-float.png", class: "medium-icon")
     elsif medium == "Other"
+      image_tag("podcast-float.png", class: "medium-icon")
     end
   end
 end
