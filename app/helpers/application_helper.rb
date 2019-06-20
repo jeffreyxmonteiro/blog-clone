0c29290e8ebbd2a1d9d0cfa7a7d120d266f538a3
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-06-19 22:14:13 -0400

    Initial refactor of helper method and assets name update.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,19 +2,19 @@ module ApplicationHelper
 
   def medium_helper medium
     if medium == "Podcast"
-      image_tag("podcast-float.png", class: "medium-icon")
+      image_tag("podcast-medium.png", class: "medium-icon")
     elsif medium == "Tweet"
-      image_tag("twitter-float.png", class: "medium-icon")
+      image_tag("twitter-medium.png", class: "medium-icon")
     elsif medium == "Book"
-      image_tag("book-float.png", class: "medium-icon")
+      image_tag("book-medium.png", class: "medium-icon")
     elsif medium == "Video"
-      image_tag("youtube-float.png", class: "medium-icon")
+      image_tag("youtube-medium.png", class: "medium-icon")
     elsif medium == "Online Article"
-      image_tag("article-float.png", class: "medium-icon")
+      image_tag("article-medium.png", class: "medium-icon")
     elsif medium == "Newsletter"
-      image_tag("mail-float.png", class: "medium-icon")
+      image_tag("mail-medium.png", class: "medium-icon")
     elsif medium == "Other"
-      image_tag("podcast-float.png", class: "medium-icon")
+      image_tag("podcast-medium.png", class: "medium-icon")
     end
   end
 end
