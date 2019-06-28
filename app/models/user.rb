f67dd643361217b204a36d3b9548c4629df91f98
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-06-28 18:03:14 -0400

    Added acts as tagger to user model. Implementation unfinished.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  acts_as_tagger
   has_many :snipps, dependent: :destroy
   validates_presence_of :fname, :lname
 end
