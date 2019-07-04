bcceb2202bc4f08f4b0be77ed4c41059dd26ce15
jeffrey monteiro <jeffreyxmonteiro@gmail.com>
2019-07-04 03:48:35 -0400

    Added top tags - first successful implementation for user.

--- app/models/snipp.rb
+++ app/models/snipp.rb
@@ -10,4 +10,13 @@ class Snipp < ApplicationRecord
   belongs_to :user
   validates_presence_of :medium, :quotation, :takeaway, :quoter, :source
   acts_as_taggable_on :tags
+
+  before_save :set_tag_owner
+  def set_tag_owner
+      # Set the owner of some tags based on the current tag_list
+      set_owner_tag_list_on(user, :tags, self.tag_list)
+      # Clear the list so we don't get duplicate taggings
+      self.tag_list = nil
+   end
+
 end
